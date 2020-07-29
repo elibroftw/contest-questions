70b45b8af44a9720532a80ea31bee64d316b8db7
Elijah Lopez <elijahllopezz@gmail.com>
2020-07-29 16:12:46 -0400

    Less global variables

--- CCC/2012/J5 A Coin Game.py
+++ CCC/2012/J5 A Coin Game.py
@@ -3,29 +3,34 @@
 # since a lookup for a set is O(1) v. list which is O(n)
 
 
-def do_left(left):
-    global new_positions, coin_lineup, coin_stack
-    new_positions = None
-    left = coin_lineup[left]
-    if left == '':
-        new_positions = coin_lineup[:i - 1] + (coin_stack[0],) + (coin_stack[1:],) + coin_lineup[i + 1:]
-    elif int(coin_stack[0]) < int(left[0]):
-        new_positions = coin_lineup[:i - 1] + (coin_stack[0] + coin_lineup[i - 1],) + (coin_stack[1:],) + coin_lineup[i + 1:]
-
-def do_right(right):
-    global new_positions
-    new_positions = None
-    right = coin_lineup[right]
-    if right == '':
-        new_positions = coin_lineup[:i] + (coin_stack[1:],) + (coin_stack[0],) + coin_lineup[i + 2:]
-    elif int(coin_stack[0]) < int(right[0]):
-        new_positions = coin_lineup[:i] + (coin_stack[1:],) + (coin_stack[0] + coin_lineup[i + 1],) + coin_lineup[i + 2:]
+def stack_empty(stack: str):
+    """ returns whether the coin stack is empty """
+    return stack == ''
 
 
-def create_move(other_side, this_side_visited):
-    global done, new_configs, new_positions
-    if new_positions is not None:
-        hashable = tuple(new_positions)
+def do_left(coin_lineup, coin_stack, centre_stack_index):
+    i = centre_stack_index
+    left_stack = coin_lineup[i - 1]
+    if stack_empty(left_stack):
+        return coin_lineup[:i - 1] + (coin_stack[0], coin_stack[1:]) + coin_lineup[i + 1:]
+    elif int(coin_stack[0]) < int(left_stack[0]):
+        return coin_lineup[:i - 1] + (coin_stack[0] + coin_lineup[i - 1], coin_stack[1:]) + coin_lineup[i + 1:]
+
+
+def do_right(coin_lineup, coin_stack, centre_stack_index):
+    # global coin_lineup, coin_stack
+    i = centre_stack_index
+    right_stack = coin_lineup[i + 1]
+    if stack_empty(right_stack):
+        return coin_lineup[:i] + (coin_stack[1:], coin_stack[0]) + coin_lineup[i + 2:]
+    elif int(coin_stack[0]) < int(right_stack[0]):
+        return coin_lineup[:i] + (coin_stack[1:], coin_stack[0] + coin_lineup[i + 1]) + coin_lineup[i + 2:]
+
+
+def create_move(other_side, this_side_visited, new_lineup):
+    global done, new_configs
+    if new_lineup is not None:
+        hashable = tuple(new_lineup)
         if hashable not in this_side_visited:
             if hashable in other_side:
                 print(step)
@@ -58,18 +63,17 @@ while n > 0:
             step += 1
             new_configs = set()
             for coin_lineup in lines_forward:
-                # positions = positions.split(' ')
                 if done: break
                 for i, coin_stack in enumerate(coin_lineup):
                     if coin_stack:
                         if i > 0:
-                            do_left(i - 1)
-                            if create_move(lines_backward, visited_from_start):
+                            possible_lineup = do_left(coin_lineup, coin_stack, i)
+                            if create_move(lines_backward, visited_from_start, possible_lineup):
                                 break
 
                         if i < n - 1:
-                            do_right(i + 1)
-                            if create_move(lines_backward, visited_from_start):
+                            possible_lineup = do_right(coin_lineup, coin_stack, i)
+                            if create_move(lines_backward, visited_from_start, possible_lineup):
                                 break
             lines_forward = new_configs.copy()
 
@@ -77,17 +81,16 @@ while n > 0:
             new_configs = set()
             for coin_lineup in lines_backward:
                 if done: break
-                # positions = positions.split(' ')
                 for i, coin_stack in enumerate(coin_lineup):
                     if coin_stack:
                         if i > 0:
-                            do_left(i - 1)
-                            if create_move(lines_forward, visited_from_back):
+                            possible_lineup = do_left(coin_lineup, coin_stack, i)
+                            if create_move(lines_forward, visited_from_back, possible_lineup):
                                 break
 
                         if i < n - 1:
-                            do_right(i + 1)
-                            if create_move(lines_forward, visited_from_back):
+                            possible_lineup = do_right(coin_lineup, coin_stack, i)
+                            if create_move(lines_forward, visited_from_back, possible_lineup):
                                 break
 
             lines_backward = new_configs.copy()
