093658f721ed73ccaa4689c80b9dd30e1796a131
elibroftw <xxelibroxx@gmail.com>
2020-09-05 13:41:14 -0400

    Optimized solution

--- ICPC Live Archive/5825 Non-negative Partial Sums.py
+++ ICPC Live Archive/5825 Non-negative Partial Sums.py
@@ -1,10 +1,9 @@
+# Author: Elijah Lopez
 # https://icpcarchive.ecs.baylor.edu/index.php?option=com_onlinejudge&Itemid=8&category=530&page=show_problem&problem=3836
-
-import random
-import time
 
 
 def solve_old(n, integers):
+    # v2 solution. TLE
     iters = 0
     sums = {}
     valid_keys = set()
@@ -35,96 +34,51 @@ def solve_old(n, integers):
                     keys_to_ignore.add(k)
                 else: sums[k] += integer
         if i in valid_keys: valid_keys.remove(i)
-    print(iters, 'iters v1')
     return len(sums)
 
 
 def solve(n, integers):
+    # v3 solution. Optimized and passes
     iters = 0
     valid_seqs = n
-    total_sum = 0
-    went_back = {}
-    sum_cache = {}  # 'i:j': sum
+    sum_cache = {}  # 'i:j': (index_stuck_at, sum)
+    skip_cache = set()
     for i, integer in enumerate(integers):
         iters += 1
+        integer = int(integer)
+        if i in skip_cache: break  # short circuit
         if integer < 0:
             valid_seqs -= 1
-            temp_sum = 0
-            for j in range(1, len(integers)):
-                iters += 1
-                temp_num = integers[i - j]
+            temp_sum = integer
+            j = i - 1
+            max_index = i - n
+            while j > max_index:
+                temp_num = int(integers[j])  # a previous integer
+                if temp_num < 0:
+                    if (j % n) in sum_cache:
+                        temp_sum += sum_cache[j % n][1]
+                        temp_j = sum_cache[j % n][0]
+                        if temp_j > j: temp_j -= n
+                        j = temp_j
+                        temp_num = int(integers[j])
+                skip_cache.add(j % n)
                 temp_sum += temp_num
-                if temp_sum + integer >= 0:
-                    went_back[i] = ((i - j) + len(integers)) % len(integers)
-                    print(went_back[i])
+                if temp_sum >= 0:
+                    sum_cache[i] = (j, temp_sum - temp_num)
+                    # sum is positive again
+                    #  would become negative if a bigger negative integer is before but that would have
+                    #  triggered this kind of search and so we break to avoid double counting
                     break
-                if temp_num >= 0: valid_seqs -= 1
-        total_sum += integer
-        # print(iters)
-    # print(valid_seqs)
-    # print(total_sum)
-    if total_sum < 0: return 0
-    # print(iters, 'iters v2')
+                valid_seqs -= 1
+                j -= 1
+            if j <= max_index: return 0  # short circuit
     return valid_seqs
 
 
-def solvev2(n, integers):
-    iters = 0
-    valid_seqs = n
-    total_sum = 0
-    for i, integer in enumerate(integers):
-        iters += 1
-        if integer < 0:
-            valid_seqs -= 1
-            temp_sum = 0
-            for j in range(1, len(integers)):
-                iters += 1
-                temp_num = integers[i - j]
-                temp_sum += temp_num
-                if temp_sum + integer >= 0: break
-                if temp_num >= 0: valid_seqs -= 1
-        total_sum += integer
-        # print(iters)
-    if total_sum < 0: return 0
-    print(iters, 'iters v2')
-    return valid_seqs
-
-
-# assert solve(5, [1, 1, 1, -5, -9]) == 0
-assert solve_old(5, [14, 1, 1, -5, -9]) == 1
-assert solve(5, [14, 1, 1, -5, -9]) == 1
-assert solve(3, [2, 2, 1]) == 3
-assert solve(3, [3, -3, 1]) == 2
-assert solve(3, [-3, 3, 1]) == 1
-assert solve(3, [2, 2, 0]) == 3
-assert solve(3, [-1, 1, 1]) == 2
-assert solve(3, [-1, -2, -5]) == 0
-assert solve(1, [-1]) == 0
-assert solve(2, [-1, -2]) == 0
-assert solve(3, [-1, -2, -5]) == 0
-assert solve(3, [0, 0, 0]) == 3
-assert solve(3, [0, 0, -1]) == 0
-assert solve(1, [0]) == 1
-assert solve(3, [10, -5, -5]) == 1
-assert solve(3, [9, -5, -5]) == 0
-assert solve(4, [9, -5, -5, 1]) == 1
-assert solve(4, [9, -5, -6, 1]) == 0
-assert solve(4, [1000, -1000, -1000, 1000]) == 1
-assert solve(4, [1000, 1000, -1000, -1000]) == 1
-assert solve(4, [-1000, 1000, 1000, -1000]) == 1
-assert solve(4, [1000, -1000, 1000, -1000]) == 2
-assert solve(3, [1000, -1000, 0]) == 2
-print('10 ** 4')
-solve(10 ** 4, [1000 if random.randint(0, 2) else -1000 for _ in range(10 ** 4)])
-print('10 ** 5')
-solve(10 ** 5, [1000 if random.randint(0, 2) else -1000 for _ in range(10 ** 5)])
-print('10 ** 6')  # max length list # TLE test
-solve(10 ** 6, [1000 if random.randint(0, 1) else -1000 for _ in range(10 ** 6)])
-print('passed tests')
-
-n = 1
-while True:
-    n = int(input())
-    if not n: break
-    integers = [int(x) for x in input().split()]
-    print(solve(n, integers))
+if __name__ == '__main__':
+    n = 1
+    while True:
+        n = int(input())
+        if not n: break
+        # integers = [int(x) for x in input().split()]
+        print(solve(n, input().split()))
---
+++ ICPC Live Archive/5825 test_harness.py
@@ -0,0 +1,37 @@
+import random
+import time
+solution = __import__('5825 Non-negative Partial Sums')
+solve = solution.solve
+
+
+assert solve(5, [14, 1, 1, -5, -9]) == 1
+assert solve(5, [1, 1, 1, -5, -9]) == 0
+assert solve(3, [2, 2, 1]) == 3
+assert solve(3, [3, -3, 1]) == 2
+assert solve(3, [-3, 3, 1]) == 1
+assert solve(3, [2, 2, 0]) == 3
+assert solve(3, [-1, 1, 1]) == 2
+assert solve(3, [-1, -2, -5]) == 0
+assert solve(1, [-1]) == 0
+assert solve(2, [-1, -2]) == 0
+assert solve(3, [-1, -2, -5]) == 0
+assert solve(3, [0, 0, 0]) == 3
+assert solve(3, [0, 0, -1]) == 0
+assert solve(1, [0]) == 1
+assert solve(3, [10, -5, -5]) == 1
+assert solve(3, [9, -5, -5]) == 0
+assert solve(4, [9, -5, -5, 1]) == 1
+assert solve(4, [9, -5, -6, 1]) == 0
+assert solve(4, [1000, -1000, -1000, 1000]) == 1
+assert solve(4, [1000, 1000, -1000, -1000]) == 1
+assert solve(4, [-1000, 1000, 1000, -1000]) == 1
+assert solve(4, [1000, -1000, 1000, -1000]) == 2
+assert solve(3, [1000, -1000, 0]) == 2
+solve(10 ** 4, [1000 if random.randint(0, 2) else -1000 for _ in range(10 ** 4)])  # 10 ** 4
+solve(10 ** 5, [1000 if random.randint(0, 2) else -1000 for _ in range(10 ** 5)])  # 10 ** 5
+for x in range(10):
+    # length = 10 ** 6
+    hard_lst = [random.randint(-1000, 1000) for _ in range(10 ** 6)]
+    t1 = time.time()
+    print(solve(10 ** 6, hard_lst))
+    print(time.time() - t1)
