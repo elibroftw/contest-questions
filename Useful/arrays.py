f5d66647169035583cd62a883114c15b84db8d7b
Elijah Lopez <elijahllopezz@gmail.com>
2021-10-14 22:22:44 -0400

    Create arrays.py

---
+++ Useful/arrays.py
@@ -0,0 +1,18 @@
+"""
+Tips:
+
+Use a dictionary to keep track of what we need to look for
+(Two Sum, 3 Sum)
+
+Binary Search (modified)
+
+Attack from two ends
+(Water Container, Binary Searches)
+Start at left and right and work towards middle
+
+Subarrays
+Kadane's algorithm
+- Only update current if before meets criteria
+Another way is to keep track of max so far
+and using a cur_maximum that resets if it goes below 0.
+"""
